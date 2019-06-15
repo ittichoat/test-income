2f79e8b2268ffb814913f7d48ae592ac17a2c999
ittichoat <37753672+ittichoat@users.noreply.github.com>
2019-06-15 13:55:12 +0700

    Update users.js

--- controller/users.js
+++ controller/users.js
@@ -1,9 +1,16 @@
 async function create(id) {
+    //let body = ctx.request.body
     if (id) {
-        await database.users.create({
-            id: id,
-        })
-        console.log('OK')
+        let userid = await database.users.findOne({ id: id })
+        if (!userid) {
+            await database.users.create({
+                id: id,
+            })
+            console.log('OK')
+        }
+        else {
+            console.log('error')
+        }
     }
     else {
         console.log('error')
